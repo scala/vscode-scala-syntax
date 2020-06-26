05a24a615db6622ec99c5bccbf982d83b30728d4
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-06-26 17:14:00 +0200

    Fix #68 and fix #77: End names on open brackets

--- src/typescript/Scala.tmLanguage.ts
+++ src/typescript/Scala.tmLanguage.ts
@@ -568,7 +568,7 @@ export const scalaTmLanguage: TmLanguage = {
           }
         },
         {
-          match: '\\b(?:(case|open)\\s+)?(class|object)\\s+([^\\s\\{\\(\\[]+)',
+          match: '\\b(?:(case|open)\\s+)?(class|object)\\s+([^\\s\\{\\(\\[;]+)',
           captures: {
             '1': {
               name: 'keyword.declaration.scala'
@@ -935,7 +935,7 @@ export const scalaTmLanguage: TmLanguage = {
     inheritance: {
       patterns: [
         {
-          match: '(extends|with|derives)\\s+([^\\s\\(\\{\\[\\]]+|(?=\\([^\\)]+=>))',
+          match: '(extends|with|derives)\\s+([^\\s\\(\\{\\[\\]\\)\\};]+|(?=\\([^\\)]+=>))',
           captures: {
             '1': {
               name: 'keyword.declaration.scala'
---
+++ tests/unit/#77.test.scala
@@ -0,0 +1,15 @@
+// SYNTAX TEST "source.scala"
+
+
+class A extends B with NoStackTrace)
+//                ^^^^ keyword.declaration.scala
+//                     ^^^^^^^^^^^^ entity.other.inherited-class.scala
+//                                 ^ meta.bracket.scala
+class A extends B with NoStackTrace}
+//                ^^^^ keyword.declaration.scala
+//                     ^^^^^^^^^^^^ entity.other.inherited-class.scala
+//                                 ^ punctuation.section.block.end.scala
+class A extends B with NoStackTrace]
+//                ^^^^ keyword.declaration.scala
+//                     ^^^^^^^^^^^^ entity.other.inherited-class.scala
+//                                 ^ meta.bracket.scala
