c44565664193b37f063319c8c1d3466de5ba8c4b
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-06-26 21:21:39 +0200

    Fix #84: Support with followed by a String literal

---
+++ tests/unit/#84.test.scala
@@ -0,0 +1,11 @@
+// SYNTAX TEST "source.scala"
+
+
+   Type[A with "user provided string" with B]
+//      ^ entity.name.class
+//        ^^^^ keyword.declaration.scala
+//             ^ string.quoted.double.scala punctuation.definition.string.begin.scala
+//              ^^^^^^^^^^^^^^^^^^^^ string.quoted.double.scala
+//                                  ^ string.quoted.double.scala punctuation.definition.string.end.scala
+//                                    ^^^^ keyword.declaration.scala
+//                                         ^ entity.other.inherited-class.scala
