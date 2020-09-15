806d9a7ce07a64e3000a094c99181017b687ff80
Nicolas Stucki <nicolas.stucki@gmail.com>
2020-09-15 09:43:34 +0200

    Fix #133: Add missing boundary condition
    This fixes some cornercases of the previous fix for #133.

--- tests/unit/#133.test.scala
+++ tests/unit/#133.test.scala
@@ -18,6 +18,21 @@
 //  ^^^^^^ keyword.control.flow.scala
 //           ^^^^^ keyword.control.flow.scala
 
+    inline xval match {
+//  ^^^^^^ keyword.control.flow.scala
+//              ^^^^^ keyword.control.flow.scala
+
+    inline val1 match {
+//  ^^^^^^ keyword.control.flow.scala
+//              ^^^^^ keyword.control.flow.scala
+
+    inline def1 match {
+//  ^^^^^^ keyword.control.flow.scala
+//              ^^^^^ keyword.control.flow.scala
+
+    inline given1 match {
+//  ^^^^^^ keyword.control.flow.scala
+//                ^^^^^ keyword.control.flow.scala
 
     inline def power(x: Double, inline N: Int): Double =
 //  ^^^^^^ storage.modifier.other
@@ -40,6 +55,22 @@ val x = inline[T]
 //                    ^^^^^^ storage.modifier.other
 //                           ^^^^^^ variable.parameter.scala
 
+    inline def inline(inline xif: Int, inline ifx: Int): Double =
+//  ^^^^^^ storage.modifier.other
+//             ^^^^^^ entity.name.function.declaration
+//                    ^^^^^^ storage.modifier.other
+//                           ^^^ variable.parameter.scala
+//                                     ^^^^^^ storage.modifier.other
+//                                            ^^^ variable.parameter.scala
+
+    inline def inline(inline xmatch: Int, inline matchx: Int): Double =
+//  ^^^^^^ storage.modifier.other
+//             ^^^^^^ entity.name.function.declaration
+//                    ^^^^^^ storage.modifier.other
+//                           ^^^^^^ variable.parameter.scala
+//                                        ^^^^^^ storage.modifier.other
+//                                               ^^^^^^ variable.parameter.scala
+
    inline if (n == 0) 1 else 2; val x = 2
 // ^^^^^^ keyword.control.flow.scala
 //        ^^ keyword.control.flow.scala
@@ -51,3 +82,21 @@ val x = inline[T]
     inline f[X](x: X) match {
 //  ^^^^^^ keyword.control.flow.scala
 //                    ^^^^^ keyword.control.flow.scala
+
+    inline xval
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
+
+    inline valx
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
+
+    inline defx
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
+
+    inline xdef
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
+
+    inline givenx
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
+
+    inline xgiven
+//  ^^^^^^ - storage.modifier.other keyword.control.flow.scala
