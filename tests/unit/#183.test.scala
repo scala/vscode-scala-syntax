3bbb8bd2a684902ee5f19cacd8da97b9d2080062
Nicolas Stucki <nicolas.stucki@epfl.ch>
2021-01-17 13:53:55 +0100

    Fix comments

--- tests/unit/#183.test.scala
+++ tests/unit/#183.test.scala
@@ -14,14 +14,14 @@
 //      ^^ - constant.character.escape.scala
 //        ^ punctuation.definition.string.end.scala
    
-    raw"$$ " // `$"` is an escaped `"` in raw interpolators
+    raw"$$ " // `$$` is an escaped `$` in raw interpolators
 //  ^^^ source.scala keyword.interpolation.scala
 //     ^ string.quoted.double.interpolated.scala punctuation.definition.string.begin.scala
 //      ^^ constant.character.escape.scala
 //        ^ string.quoted.double.interpolated.scala
 //         ^ punctuation.definition.string.end.scala
 
-    raw"$" " // `$$` is an escaped `$` in raw interpolators
+    raw"$" " // `$"` is an escaped `"` in raw interpolators
 //  ^^^ source.scala keyword.interpolation.scala
 //     ^ string.quoted.double.interpolated.scala punctuation.definition.string.begin.scala
 //      ^^ constant.character.escape.scala
