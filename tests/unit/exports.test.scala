d680933ad74696c29373cdb4735eee4a1a897a18
Tomasz Godzik <tgodzik@virtuslab.com>
2023-02-08 21:15:51 +0100

    bugfix: Adjust export syntax to newest

--- tests/unit/exports.test.scala
+++ tests/unit/exports.test.scala
@@ -44,46 +44,52 @@
 
     export given
 //  ^^^^^^ meta.export.scala keyword.other.export.scala
-//         ^^^^^ meta.export.scala
+//         ^^^^^ keyword.other.export.given.scala
 
-    export given a.x
+    export a.given
 //  ^^^^^^ meta.export.scala keyword.other.export.scala
-//         ^^^^ meta.export.scala
-//              ^ meta.export.scala
-//               ^ meta.export.scala entity.name.export.scala
-//                ^ meta.export.scala punctuation.definition.export
-//                 ^ meta.export.scala entity.name.export.scala
-
-    export given a._
-//  ^^^^^^ meta.export.scala keyword.other.export.scala
-//         ^^^^^ meta.export.scala
-//              ^ meta.export.scala
-//               ^ meta.export.scala entity.name.export.scala
-//                ^ meta.export.scala punctuation.definition.export
-//                 ^ meta.export.scala entity.name.export.scala
+//        ^ meta.export.scala
+//         ^ meta.export.scala entity.name.export.scala
+//          ^ meta.export.scala punctuation.definition.export
+//           ^^^^^ meta.export.scala keyword.other.export.given.scala
     
-    export given a.{x, y}
+    export a.{x, y, given}
 //  ^^^^^^ meta.export.scala keyword.other.export.scala
-//         ^^^^^ meta.export.scala
-//              ^ meta.export.scala
-//               ^ meta.export.scala entity.name.export.scala
-//                ^ meta.export.scala punctuation.definition.export
-//                 ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
-//                  ^ meta.export.scala entity.name.export.scala
-//                   ^^ meta.export.scala meta.export.selector.scala
-//                     ^ meta.export.scala meta.export.selector.scala entity.name.export.scala
-//                      ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+//        ^ meta.export.scala
+//         ^ meta.export.scala entity.name.export.scala
+//          ^ meta.export.scala punctuation.definition.export
+//           ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+//            ^ meta.export.scala entity.name.export.scala
+//             ^^ meta.export.scala meta.export.selector.scala
+//               ^ meta.export.scala meta.export.selector.scala entity.name.export.scala
+//                ^^ source.scala meta.export.scala meta.export.selector.scala
+//                  ^^^^^ keyword.other.export.given.scala 
+//                       ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
 
-    export given a.{x => y}
+    export a.{x => y, given}
 //  ^^^^^^ meta.export.scala keyword.other.export.scala
-//         ^^^^^ meta.export.scala
-//              ^ meta.export.scala
-//               ^ meta.export.scala entity.name.export.scala
-//                ^ meta.export.scala punctuation.definition.export
-//                 ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
-//                  ^ meta.export.scala meta.export.selector.scala entity.name.export.renamed-from.scala
+//        ^ meta.export.scala
+//         ^ meta.export.scala entity.name.export.scala
+//          ^ meta.export.scala punctuation.definition.export
+//           ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+//            ^ meta.export.scala meta.export.selector.scala entity.name.export.renamed-from.scala
+//             ^ meta.export.scala meta.export.selector.scala
+//              ^^ meta.export.scala meta.export.selector.scala
+//                ^ meta.export.scala meta.export.selector.scala
+//                 ^ meta.export.scala meta.export.selector.scala entity.name.export.renamed-to.scala
 //                   ^ meta.export.scala meta.export.selector.scala
-//                    ^^ meta.export.scala meta.export.selector.scala
-//                      ^ meta.export.scala meta.export.selector.scala
-//                       ^ meta.export.scala meta.export.selector.scala entity.name.export.renamed-to.scala
-//                        ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+//                    ^^^^^ keyword.other.export.given.scala 
+//                         ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+
+    export A.{a, given Ordering[?]}
+//  ^^^^^^ meta.export.scala keyword.other.export.scala
+//        ^ meta.export.scala
+//         ^ meta.export.scala entity.name.class.export.scala
+//          ^ meta.export.scala punctuation.definition.export
+//           ^ meta.export.scala meta.export.selector.scala meta.bracket.scala
+//            ^ meta.export.scala entity.name.export.scala
+//             ^^ meta.export.scala meta.export.selector.scala
+//               ^^^^^ meta.export.scala keyword.other.export.given.scala
+//                    ^ meta.export.scala meta.export.selector.scala
+//                     ^^^^^^^^ meta.export.scala entity.name.class.export.scala
+//                             ^^^^ meta.export.scala meta.export.selector.scala
